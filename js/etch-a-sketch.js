25dca2f26f00fb12de4db5c43acc83df65602321
Kenya Chan <kenya.chan@gmail.com>
2022-08-28 00:01:01 +1000

    Add event listener to grid items

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,14 +2,15 @@ function createGrid(size) {
     let grid = document.querySelector('#grid-container');
     
     grid.style.gridTemplateColumns = `repeat(${Math.sqrt(size)}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${Math.sqrt(size)}, 1fr)`;
 
     for (let i = 0; i < size; i++) {
         let gridItem = document.createElement('div')
         
         gridItem.classList.add('grid-item');
+        gridItem.addEventListener('mouseover', draw);
         grid.append(gridItem);
     }
-
 }
 
 function isSquare(number) {
@@ -18,4 +19,8 @@ function isSquare(number) {
     return Number.isInteger(Math.sqrt(number)) ? true : false;
 }
 
-createGrid(4096);
+function draw(gridItem) {
+    gridItem.target.classList.add('drawn');
+}
+
+createGrid(4);
