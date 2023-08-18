a5f44c4df8867cef5cfd3caa35ace19d95704cd6
Kenya Chan <kenya.chan@gmail.com>
2023-08-18 14:15:05 +1000

    refactor

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,7 +2,6 @@ function createGrid(size) {
     let grid = document.querySelector('#grid-container');
     
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < Math.pow(size, 2); i++) {
         let gridItem = document.createElement('div')
